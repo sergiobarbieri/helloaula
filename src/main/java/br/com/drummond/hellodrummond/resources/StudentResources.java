788651d5767a6dbc180a7b4e7b437807f705e09d
Administrador <Administrador@LAB7A4.labtatuape.drummond.com.br>
2018-03-28 21:50:16 -0300

    Prototipo CRUD

--- src/main/java/br/com/drummond/hellodrummond/resources/StudentResources.java
+++ src/main/java/br/com/drummond/hellodrummond/resources/StudentResources.java
@@ -3,6 +3,7 @@ package br.com.drummond.hellodrummond.resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,9 @@ public class StudentResources {
 		return ResponseEntity.status(HttpStatus.OK).body(retorno);
 	}
 	
+	/*
+	 * Obter todos os alunos
+	 */
 	@RequestMapping(value= {"/alunos"}, method = RequestMethod.GET)
 	public ResponseEntity<?> obterAlunos()
 	{
@@ -26,6 +30,9 @@ public class StudentResources {
 		return ResponseEntity.status(HttpStatus.OK).body(retorno);
 	}
 	
+	/*
+	 * Obter aluno especifico
+	 */
 	
 	@RequestMapping(value= {"/alunos/{ra}"}, method = RequestMethod.GET)
 	public ResponseEntity<?> obterAlunosRa(@PathVariable("ra") Long idRA )
@@ -44,6 +51,47 @@ public class StudentResources {
 		aluno.setEmail("mrjean@company.uk");
 		
 		return ResponseEntity.status(HttpStatus.OK).body(aluno);
+	}
+	
+	/*
+	 * Novo aluno
+	 */
+	
+	@RequestMapping(value= {"/alunos"}, method = RequestMethod.POST)
+	public ResponseEntity<?> criarAlunosRa(@RequestBody Aluno aluno )
+	{
+		
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(aluno);
+	}
+	
+	
+	/*
+	 * Alterar aluno existente
+	 */
+	
+	@RequestMapping(value= {"/alunos"}, method = RequestMethod.PUT)
+	public ResponseEntity<?> alterarAlunosRa(@RequestBody Aluno aluno )
+	{
+		
+		return ResponseEntity.status(HttpStatus.OK).body(aluno);
+	}
+	
+	/*
+	 * Eliminar aluno
+	 */
+	
+	@RequestMapping(value= {"/alunos/{ra}"}, method = RequestMethod.DELETE)
+	public ResponseEntity<?> eliminarAlunosRa(@PathVariable("ra") Long idRA )
+	{
+		Aluno aluno = new Aluno();
+		if (idRA > 99999999)
+		{
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+		}
+	
+		
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(aluno);
 	}
 }
 
