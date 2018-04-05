c94480dfaacc4fabaca86481f688c8d55e616617
Administrador <Administrador@LAB7A4.labtatuape.drummond.com.br>
2018-04-05 20:51:15 -0300

    oracle

--- src/main/java/br/com/drummond/hellodrummond/HellodrummondApplication.java
+++ src/main/java/br/com/drummond/hellodrummond/HellodrummondApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 
 @SpringBootApplication
 public class HellodrummondApplication {
